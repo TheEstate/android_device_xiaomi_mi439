b3280cb8eb047350f33eb3f4e2769fc04c307e63
Estatè <86308945+TheEstate@users.noreply.github.com>
2022-04-02 22:37:52 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mi439.mk
+    $(LOCAL_DIR)/lighthouse_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mi439-user \
-    lineage_mi439-userdebug \
-    lineage_mi439-eng
+    lighthouse_mi439-user \
+    lighthouse_mi439-userdebug \
+    lighthouse_mi439-eng
